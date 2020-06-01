dc215e2926b029493fbc347c4673d8c27641af6a
Gereon Kremer <gkremer@stanford.edu>
2020-06-01 23:05:47 +0200

    Added documentation for Assignment wrapper.

--- src/theory/arith/nl/libpoly/assignment.h
+++ src/theory/arith/nl/libpoly/assignment.h
@@ -15,31 +15,40 @@ namespace theory {
 namespace nlarith {
 namespace libpoly {
 
+    /** A deleter for an std::unique_ptr holding a lp_assignment_t pointer */
     inline void assignment_deleter(lp_assignment_t* ptr) {
         lp_assignment_delete(ptr);
     }
 
+    /**
+     * Implements a wrapper for lp_assignment_t from libpoly.
+     */
     class Assignment {
         friend std::ostream& operator<<(std::ostream& os, const Assignment& a);
+        /** The actual assignment. */
         deleting_unique_ptr<lp_assignment_t> mAssignment;
     public:
+        /** Construct an empty assignment. */
         Assignment(): mAssignment(lp_assignment_new(variable_db.get()), assignment_deleter) {}
 
+        /** Get a non-const pointer to the internal lp_assignment_t. Handle with care! */
         lp_assignment_t* get() {
             return mAssignment.get();
         }
+        /** Get a const pointer to the internal lp_assignment_t. */
         const lp_assignment_t* get() const {
             return mAssignment.get();
         }
 
+        /** Assign var to the given value. */
         void set(const Variable& var, const Value& value) {
             lp_assignment_set_value(get(), var.get(), value.get());
         }
     };
+    /** Stream the given Assignment to an output stream. */
     inline std::ostream& operator<<(std::ostream& os, const Assignment& a) {
         return os << lp_assignment_to_string(a.get());
     }
-
 
 }
 }
