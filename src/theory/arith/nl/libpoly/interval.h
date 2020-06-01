8824930d44109e11f88ae48290cf99fc5fd71ffc
Gereon Kremer <gkremer@stanford.edu>
2020-06-01 23:06:04 +0200

    Working on Interval wrapper, added documentation.

--- src/theory/arith/nl/libpoly/interval.h
+++ src/theory/arith/nl/libpoly/interval.h
@@ -2,6 +2,7 @@
 #ifndef CVC4__THEORY__NLARITH__LIBPOLY__INTERVAL_H
 #define CVC4__THEORY__NLARITH__LIBPOLY__INTERVAL_H
 
+#include "integer.h"
 #include "utils.h"
 
 #include <poly/dyadic_interval.h>
@@ -13,40 +14,50 @@ namespace theory {
 namespace nlarith {
 namespace libpoly {
 
+    /**
+     * Implements a wrapper for lp_dyadic_interval_t from libpoly.
+     */
     class Interval {
         friend std::ostream& operator<<(std::ostream& os, const Interval& i);
     private:
+        /** The actual interval. */
         lp_dyadic_interval_t mInterval;
     public:
-//        Polynomial(): mPoly(lp_polynomial_new(polynomial_ctx.get()), polynomial_deleter) {}
-//        Polynomial(lp_polynomial_t* poly): mPoly(poly, polynomial_deleter) {}
-//        Polynomial(int i, Variable v, unsigned n): mPoly(lp_polynomial_alloc(), polynomial_deleter) {
-//            lp_integer_t it;
-//            lp_integer_construct_from_int(lp_Z, &it, i);
-//            lp_polynomial_construct_simple(get(), polynomial_ctx.get(), &it, v.get(), n);
-//            lp_integer_destruct(&it);
-//        }
-        Interval(int a, int b) {
-            lp_dyadic_interval_construct_from_int(&mInterval, a, 1, b, 1);
+        /** Disallow the default constructor. */
+        Interval() = delete;
+        /** Construct an open interval from the given two intervals. */
+        Interval(const Integer& a, const Integer& b) {
+            lp_dyadic_interval_construct_from_integer(&mInterval, a.get(), 1, b.get(), 1);
         }
+        /** Copy from the given Interval. */
+        Interval(const Interval& i) {
+            lp_dyadic_interval_construct_copy(&mInterval, i.get());
+        }
+        /** Custom destructor. */
         ~Interval() {
             lp_dyadic_interval_destruct(&mInterval);
         }
-
-        lp_dyadic_interval_t& get() {
-            return mInterval;
+        /** Assign from the given Interval. */
+        Interval& operator=(Interval i) {
+            std::swap(mInterval, i.mInterval);
+            return *this;
         }
-        const lp_dyadic_interval_t& get() const {
-            return mInterval;
+
+        /** Get a non-const pointer to the internal lp_dyadic_interval_t. Handle with care! */
+        lp_dyadic_interval_t* get() {
+            return &mInterval;
+        }
+        /** Get a const pointer to the internal lp_dyadic_interval_t. */
+        const lp_dyadic_interval_t* get() const {
+            return &mInterval;
         }
     };
 
-    // Standard operators
-
+    /** Stream the given Interval to an output stream. */
     inline std::ostream& operator<<(std::ostream& os, const Interval& i) {
-        os << (i.get().a_open ? "( " : "[ ");
-        os << lp_dyadic_rational_to_string(&(i.get().a)) << " ; " << lp_dyadic_rational_to_string(&(i.get().b));
-        os << (i.get().b_open ? " )" : " ]");
+        os << (i.get()->a_open ? "( " : "[ ");
+        os << lp_dyadic_rational_to_string(&(i.get()->a)) << " ; " << lp_dyadic_rational_to_string(&(i.get()->b));
+        os << (i.get()->b_open ? " )" : " ]");
         return os;
     }
 
