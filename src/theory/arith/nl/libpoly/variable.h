1c0ee0bc3bd60b87d28dcbb9bedf2c95ed660217
Gereon Kremer <gkremer@stanford.edu>
2020-06-01 23:11:13 +0200

    Added documentation for Variable.

--- src/theory/arith/nl/libpoly/variable.h
+++ src/theory/arith/nl/libpoly/variable.h
@@ -13,17 +13,24 @@ namespace theory {
 namespace nlarith {
 namespace libpoly {
 
+    /**
+     * Implements a wrapper for lp_variable_t from libpoly.
+     */
     class Variable {
         friend std::ostream& operator<<(std::ostream& os, const Variable& v);
+        /** The actual variable. */
         lp_variable_t mVariable;
     public:
+        /** Construct a new variable with the given name. */
         Variable(const char* name): mVariable(lp_variable_db_new_variable(variable_db.get(), name)) {}
 
+        /** Get the internal lp_variable_t. Note that it's only a type alias for long. */
         lp_variable_t get() const {
             return mVariable;
         }
     };
 
+    /** Stream the given Variable to an output stream. */
     inline std::ostream& operator<<(std::ostream& os, const Variable& v) {
         return os << lp_variable_db_get_name(variable_db.get(), v.get());
     }
