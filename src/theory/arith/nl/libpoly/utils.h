788985acc3dd4eb30f7b908556d1d6a5f0db723d
Gereon Kremer <gkremer@stanford.edu>
2020-06-01 23:10:36 +0200

    Added documentation for utils.

--- src/theory/arith/nl/libpoly/utils.h
+++ src/theory/arith/nl/libpoly/utils.h
@@ -15,11 +15,15 @@ namespace theory {
 namespace nlarith {
 namespace libpoly {
 
+    /** Generic type alias for a unique_ptr with a deleter function. */
     template<typename T>
     using deleting_unique_ptr = std::unique_ptr<T, std::function<void(T*)>>;
 
+    /** A central variable database. */
     extern deleting_unique_ptr<lp_variable_db_t> variable_db;
+    /** A central variable ordering. */
     extern deleting_unique_ptr<lp_variable_order_t> variable_order;
+    /** A central polynomial context. */
     extern deleting_unique_ptr<lp_polynomial_context_t> polynomial_ctx;
 
 }
