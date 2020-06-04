f5e96c967d8afb29d5ba3a7479d2092120528dab
Gereon Kremer <gkremer@stanford.edu>
2020-06-04 14:10:48 +0200

    Moving source of constraints to cpp, adding documentation.

---
+++ src/theory/arith/nl/cad/constraints.cpp
@@ -0,0 +1,192 @@
+#include "constraints.h"
+
+namespace CVC4 {
+namespace theory {
+namespace arith {
+namespace nl {
+namespace cad {
+
+using namespace libpoly;
+
+Variable Constraints::get_variable(const Node& n)
+{
+  Assert(n.getKind() == Kind::VARIABLE) << "Expect node to be a variable.";
+  auto it = mVariableMap.find(n);
+  if (it == mVariableMap.end())
+  {
+    std::string name;
+    if (!n.getAttribute(expr::VarNameAttr(), name))
+    {
+      Trace("cad-check") << "Variable " << n
+                         << " has no name, using ID instead." << std::endl;
+      name = "v_" + std::to_string(n.getId());
+    }
+    it = mVariableMap.emplace(n, Variable(name.c_str())).first;
+  }
+  return it->second;
+}
+
+bool Constraints::is_suitable_relation(Kind kind) const
+{
+  return (kind == Kind::EQUAL) || (kind == Kind::GT) || (kind == Kind::GEQ)
+         || (kind == Kind::LT) || (kind == Kind::LEQ);
+}
+
+void Constraints::normalize_denominators(Integer& d1, Integer& d2) const
+{
+  Integer g = gcd(d1, d2);
+  d1 /= g;
+  d2 /= g;
+}
+
+SignCondition Constraints::normalize_kind(Kind kind,
+                                          bool negated,
+                                          Polynomial& lhs) const
+{
+  switch (kind)
+  {
+    case Kind::EQUAL:
+    {
+      return negated ? SignCondition::NE : SignCondition::EQ;
+    }
+    case Kind::LT:
+    {
+      if (negated)
+      {
+        lhs = -lhs;
+        return SignCondition::LE;
+      }
+      return SignCondition::LT;
+    }
+    case Kind::LEQ:
+    {
+      if (negated)
+      {
+        lhs = -lhs;
+        return SignCondition::LT;
+      }
+      return SignCondition::LE;
+    }
+    case Kind::GT:
+    {
+      if (negated)
+      {
+        return SignCondition::LE;
+      }
+      lhs = -lhs;
+      return SignCondition::LT;
+    }
+    case Kind::GEQ:
+    {
+      if (negated)
+      {
+        return SignCondition::LT;
+      }
+      lhs = -lhs;
+      return SignCondition::LE;
+    }
+    default:
+      Assert(false) << "This function only deals with arithmetic relations.";
+      return SignCondition::EQ;
+  }
+}
+
+Polynomial Constraints::construct_polynomial(const Node& n,
+                                             Integer& denominator)
+{
+  denominator = Integer(1);
+  switch (n.getKind())
+  {
+    case Kind::VARIABLE:
+    {
+      return Polynomial(get_variable(n));
+    }
+    case Kind::CONST_RATIONAL:
+    {
+      Rational r = n.getConst<Rational>();
+      denominator = Integer(r.getDenominator().getValue());
+      return Polynomial(Integer(r.getNumerator().getValue()));
+    }
+    case Kind::PLUS:
+    {
+      Polynomial res;
+      Integer denom;
+      for (const auto& child : n)
+      {
+        Polynomial tmp = construct_polynomial(child, denom);
+        normalize_denominators(denom, denominator);
+        res = res * denom + tmp * denominator;
+        denominator *= denom;
+      }
+      return res;
+    }
+    case Kind::MULT:
+    case Kind::NONLINEAR_MULT:
+    {
+      Polynomial res = Polynomial(denominator);
+      Integer denom;
+      for (const auto& child : n)
+      {
+        res *= construct_polynomial(child, denom);
+        denominator *= denom;
+      }
+      return res;
+    }
+    default:
+      Trace("cad-check") << "Unhandled node " << n << " with kind "
+                         << n.getKind() << std::endl;
+  }
+  return Polynomial();
+}
+
+Polynomial Constraints::construct_constraint_polynomial(const Node& n)
+{
+  Assert(n.getNumChildren() == 2)
+      << "Supported relations only have two children.";
+  auto childit = n.begin();
+  Integer ldenom;
+  Polynomial left = construct_polynomial(*childit++, ldenom);
+  Integer rdenom;
+  Polynomial right = construct_polynomial(*childit++, rdenom);
+  Assert(childit == n.end()) << "Screwed up iterator handling.";
+  Assert(ldenom > 0 && rdenom > 0)
+      << "Expected denominators to be always positive.";
+
+  normalize_denominators(ldenom, rdenom);
+  return left * rdenom - right * ldenom;
+}
+
+void Constraints::add_constraint(const Polynomial& lhs, SignCondition sc)
+{
+  mConstraints.emplace_back(lhs, sc);
+}
+
+void Constraints::add_constraint(Node n)
+{
+  bool negated = false;
+  if (n.getKind() == Kind::NOT)
+  {
+    Assert(n.getNumChildren() == 1)
+        << "Expect negations to have a single child.";
+    negated = true;
+    n = *n.begin();
+  }
+  Assert(is_suitable_relation(n.getKind()))
+      << "Found a constraint with unsupported relation " << n.getKind();
+
+  Polynomial lhs = construct_constraint_polynomial(n);
+  SignCondition sc = normalize_kind(n.getKind(), negated, lhs);
+  Trace("cad-check") << "Parsed " << lhs << " " << sc << " 0" << std::endl;
+  add_constraint(lhs, sc);
+}
+
+const Constraints::ConstraintVector& Constraints::get_constraints() const
+{
+  return mConstraints;
+}
+
+}  // namespace cad
+}  // namespace nl
+}  // namespace arith
+}  // namespace theory
+}  // namespace CVC4
--- src/theory/arith/nl/cad/constraints.h
+++ src/theory/arith/nl/cad/constraints.h
@@ -1,7 +1,6 @@
 #ifndef CVC4__THEORY__NLARITH__CAD__CONSTRAINTS_H
 #define CVC4__THEORY__NLARITH__CAD__CONSTRAINTS_H
 
-#include <iostream>
 #include <map>
 #include <vector>
 
@@ -16,212 +15,74 @@ namespace arith {
 namespace nl {
 namespace cad {
 
-using namespace libpoly;
-
 class Constraints
 {
-  using ConstraintVector = std::vector<std::pair<Polynomial, SignCondition>>;
-  /** A list of constraints, each comprised of a polynomial and a relation
-   * symbol.
+  /** Type alias for a list of constraints. */
+  using ConstraintVector =
+      std::vector<std::pair<libpoly::Polynomial, libpoly::SignCondition>>;
+  /** A list of constraints, each comprised of a polynomial and a sign
+   * condition.
    */
   ConstraintVector mConstraints;
 
   /** A mapping from CVC4 variables to libpoly variables.
    */
-  std::map<Node, Variable> mVariableMap;
+  std::map<Node, libpoly::Variable> mVariableMap;
 
-  /** Get the corresponding variable or create a new one.
+  /** Get the corresponding libpoly variable or create a new one.
+   * Expects the given node to be a Kind::VARIABLE.
    */
-  Variable get_variable(const Node& n)
-  {
-    Assert(n.getKind() == Kind::VARIABLE) << "Expect node to be a variable.";
-    auto it = mVariableMap.find(n);
-    if (it == mVariableMap.end())
-    {
-      std::string name;
-      if (!n.getAttribute(expr::VarNameAttr(), name))
-      {
-        Trace("cad-check") << "Variable " << n
-                           << " has no name, using ID instead." << std::endl;
-        name = "v_" + std::to_string(n.getId());
-      }
-      it = mVariableMap.emplace(n, Variable(name.c_str())).first;
-    }
-    return it->second;
-  }
+  libpoly::Variable get_variable(const Node& n);
 
   /** Checks whether the given relation can be handled by CAD.
+   * Returns true if the given Kind is one of EQUAL, GT, GEQ, LT or LEQ.
    */
-  bool is_suitable_relation(Kind kind) const
-  {
-    return (kind == Kind::EQUAL) || (kind == Kind::GT) || (kind == Kind::GEQ)
-           || (kind == Kind::LT) || (kind == Kind::LEQ);
-  }
+  bool is_suitable_relation(Kind kind) const;
 
   /** Normalizes two denominators.
    * Divides both by their gcd.
    */
-  void normalize_denominators(Integer& d1, Integer& d2) const
-  {
-    Integer g = gcd(d1, d2);
-    d1 /= g;
-    d2 /= g;
-  }
+  void normalize_denominators(libpoly::Integer& d1, libpoly::Integer& d2) const;
 
-  /** Normalize the given kind, taking negation into account.
-   * Always normalizes to EQUAL, DISTINCT, LT or LEQ and adapts the lhs
-   * accordingly.
+  /** Normalize the given kind to a SignCondition, taking negation into account.
+   * Always normalizes to EQ, NE, LT or LE. Negates the polynomial is necessary.
    */
-  SignCondition normalize_kind(Kind kind, bool negated, Polynomial& lhs) const
-  {
-    switch (kind)
-    {
-      case Kind::EQUAL:
-      {
-        return negated ? SignCondition::NE : SignCondition::EQ;
-      }
-      case Kind::LT:
-      {
-        if (negated)
-        {
-          lhs = -lhs;
-          return SignCondition::LE;
-        }
-        return SignCondition::LT;
-      }
-      case Kind::LEQ:
-      {
-        if (negated)
-        {
-          lhs = -lhs;
-          return SignCondition::LT;
-        }
-        return SignCondition::LE;
-      }
-      case Kind::GT:
-      {
-        if (negated)
-        {
-          return SignCondition::LE;
-        }
-        lhs = -lhs;
-        return SignCondition::LT;
-      }
-      case Kind::GEQ:
-      {
-        if (negated)
-        {
-          return SignCondition::LT;
-        }
-        lhs = -lhs;
-        return SignCondition::LE;
-      }
-      default:
-        Assert(false) << "This function only deals with arithmetic relations.";
-        return SignCondition::EQ;
-    }
-  }
+  libpoly::SignCondition normalize_kind(Kind kind,
+                                        bool negated,
+                                        libpoly::Polynomial& lhs) const;
 
   /** Constructs a polynomial from the given node.
    *
-   * While the Node n may contain rationals, Polynomial does not.
+   * While a Node may contain rationals, a Polynomial does not.
    * We therefore also store the denominator of the returned polynomial and
    * use it to construct the integer polynomial recursively.
    * Once the polynomial has been fully constructed, we can ignore the
-   * denominator (except for its sign).
+   * denominator (except for its sign, which is always positive, though).
    */
-  Polynomial construct_polynomial(const Node& n, Integer& denominator)
-  {
-    denominator = Integer(1);
-    switch (n.getKind())
-    {
-      case Kind::VARIABLE:
-      {
-        return Polynomial(get_variable(n));
-      }
-      case Kind::CONST_RATIONAL:
-      {
-        Rational r = n.getConst<Rational>();
-        denominator = Integer(r.getDenominator().getValue());
-        return Polynomial(Integer(r.getNumerator().getValue()));
-      }
-      case Kind::PLUS:
-      {
-        Polynomial res;
-        Integer denom;
-        for (const auto& child : n)
-        {
-          Polynomial tmp = construct_polynomial(child, denom);
-          normalize_denominators(denom, denominator);
-          res = res * denom + tmp * denominator;
-          denominator *= denom;
-        }
-        return res;
-      }
-      case Kind::MULT:
-      case Kind::NONLINEAR_MULT:
-      {
-        Polynomial res = Polynomial(denominator);
-        Integer denom;
-        for (const auto& child : n)
-        {
-          res *= construct_polynomial(child, denom);
-          denominator *= denom;
-        }
-        return res;
-      }
-      default:
-        Trace("cad-check") << "Unhandled node " << n << " with kind "
-                           << n.getKind() << std::endl;
-    }
-    return Polynomial();
-  }
+  libpoly::Polynomial construct_polynomial(const Node& n,
+                                           libpoly::Integer& denominator);
 
   /** Constructs the lhs polynomial for a node representing a constraints.
    * Assumes the node to have exactly two children.
    */
-  Polynomial construct_constraint_polynomial(const Node& n)
-  {
-    Assert(n.getNumChildren() == 2)
-        << "Supported relations only have two children.";
-    auto childit = n.begin();
-    Integer ldenom;
-    Polynomial left = construct_polynomial(*childit++, ldenom);
-    Integer rdenom;
-    Polynomial right = construct_polynomial(*childit++, rdenom);
-    Assert(childit == n.end()) << "Screwed up iterator handling.";
-
-    normalize_denominators(ldenom, rdenom);
-    return left * rdenom - right * ldenom;
-  }
+  libpoly::Polynomial construct_constraint_polynomial(const Node& n);
 
  public:
-  void add_constraint(const Polynomial& lhs, SignCondition sc)
-  {
-    mConstraints.emplace_back(lhs, sc);
-  }
-  /** Add a constraints (represented by a node) to the list of constraints.
+  /** Add a constraing (represented by a polynomial and a sign condition) to the
+   * list of constraints.
    */
-  void add_constraint(Node n)
-  {
-    bool negated = false;
-    if (n.getKind() == Kind::NOT)
-    {
-      Assert(n.getNumChildren() == 1)
-          << "Expect negations to have a single child.";
-      negated = true;
-      n = *n.begin();
-    }
-    Assert(is_suitable_relation(n.getKind()))
-        << "Found a constraint with unsupported relation " << n.getKind();
+  void add_constraint(const libpoly::Polynomial& lhs,
+                      libpoly::SignCondition sc);
 
-    Polynomial lhs = construct_constraint_polynomial(n);
-    SignCondition sc = normalize_kind(n.getKind(), negated, lhs);
-    Trace("cad-check") << "Parsed " << lhs << " " << sc << " 0" << std::endl;
-    add_constraint(lhs, sc);
-  }
+  /** Add a constraints (represented by a node) to the list of constraints.
+   * The given node can either be a negation (NOT) or a suitable relation symbol
+   * as checked by is_suitable_relation().
+   */
+  void add_constraint(Node n);
 
-  const ConstraintVector& get_constraints() const { return mConstraints; }
+  /** Gives the list of added constraints.
+   */
+  const ConstraintVector& get_constraints() const;
 };
 
 }  // namespace cad
