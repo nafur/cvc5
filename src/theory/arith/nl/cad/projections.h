977aca03e273603ff1e75a832bc20d1f1a7b3688
Gereon Kremer <gkremer@stanford.edu>
2020-06-04 09:08:12 +0200

    Moving code to cpp for projections.

--- src/theory/arith/nl/cad/projections.h
+++ src/theory/arith/nl/cad/projections.h
@@ -14,62 +14,22 @@ namespace arith {
 namespace nl {
 namespace cad {
 
-using namespace libpoly;
-
 /** Sort and remove duplicates from the list of polynomials. */
-void reduce_projection_polynomials(std::vector<Polynomial>& polys)
-{
-  std::sort(polys.begin(), polys.end());
-  auto it = std::unique(polys.begin(), polys.end());
-  polys.erase(it, polys.end());
-}
+void reduce_projection_polynomials(std::vector<libpoly::Polynomial>& polys);
 
 /**
  * Adds a polynomial to the list of projection polynomials.
  * Before adding, it factorizes the polynomials and removed constant factors.
  */
-void add_polynomial(std::vector<Polynomial>& polys, const Polynomial& poly)
-{
-  for (const auto& p : square_free_factors(poly))
-  {
-    if (is_constant(p)) continue;
-    polys.emplace_back(p);
-    polys.back().simplify();
-  }
-}
+void add_polynomial(std::vector<libpoly::Polynomial>& polys, const libpoly::Polynomial& poly);
 
-void add_polynomials(std::vector<Polynomial>& polys, const std::vector<Polynomial>& p)
-{
-  for (const auto& q: p) add_polynomial(polys, q);
-}
+void add_polynomials(std::vector<libpoly::Polynomial>& polys, const std::vector<libpoly::Polynomial>& p);
 
 /**
  * Computes McCallum's projection operator.
  */
-std::vector<Polynomial> projection_mccallum(
-    const std::vector<Polynomial>& polys)
-{
-  std::vector<Polynomial> res;
-
-  for (const auto& p : polys)
-  {
-    for (const auto& coeff : coefficients(p))
-    {
-      add_polynomial(res, coeff);
-    }
-    add_polynomial(res, discriminant(p));
-  }
-  for (std::size_t i = 0; i < polys.size(); ++i)
-  {
-    for (std::size_t j = i + 1; j < polys.size(); ++j)
-    {
-      add_polynomial(res, resultant(polys[i], polys[j]));
-    }
-  }
-
-  reduce_projection_polynomials(res);
-  return res;
-}
+std::vector<libpoly::Polynomial> projection_mccallum(
+    const std::vector<libpoly::Polynomial>& polys);
 
 }  // namespace cad
 }  // namespace nl
