253e374b0b75872b815c1191bacf2c9baa687149
Gereon Kremer <gkremer@stanford.edu>
2020-08-06 14:26:28 +0200

    Moving ContractionOriginManager to separate file.

--- src/theory/arith/nl/icp/interval.h
+++ src/theory/arith/nl/icp/interval.h
@@ -10,6 +10,8 @@
 #include "util/poly_util.h"
 #include "theory/arith/nl/poly_conversion.h"
 
+#include "theory/arith/nl/icp/contraction_origins.h"
+
 namespace CVC4 {
 namespace theory {
 namespace arith {
@@ -383,74 +385,6 @@ inline std::ostream& operator<<(std::ostream& os, const IAWrapper& iaw) {
         }
     }
     return os << " }";
-}
-
-class ContractionOriginManager {
-    struct ContractionOrigin {
-        Node candidate;
-        std::vector<ContractionOrigin*> origins;
-    };
-    friend std::ostream& operator<<(std::ostream& os, const ContractionOriginManager& com);
-    friend void print(std::ostream& os, const std::string& indent, const ContractionOrigin* co);
-
-    void getOrigins(ContractionOrigin const * const origin, std::set<Node>& res) const {
-        if (!origin->candidate.isNull()) {
-            res.insert(origin->candidate);
-        }
-        for (const auto& co: origin->origins) {
-            getOrigins(co, res);
-        }
-    }
-public:
-    void add(const Node& targetVariable, const Node& candidate, const std::vector<Node>& originVariables, bool addTarget = true) {
-        Trace("nl-icp") << "Adding contraction for " << targetVariable << std::endl;
-        std::vector<ContractionOrigin*> origins;
-        if (addTarget) {
-            auto it = d_currentOrigins.find(targetVariable);
-            if (it != d_currentOrigins.end()) {
-                origins.emplace_back(it->second);
-            }
-        }
-        for (const auto& v: originVariables) {
-            auto it = d_currentOrigins.find(v);
-            if (it != d_currentOrigins.end()) {
-                origins.emplace_back(it->second);
-            }
-        }
-        d_allocations.emplace_back(new ContractionOrigin{candidate, std::move(origins)});
-        d_currentOrigins[targetVariable] = d_allocations.back().get();
-    }
-
-    std::vector<Node> getOrigins(const Node& variable) const {
-        Trace("nl-icp") << "Obtaining origins for " << variable << std::endl;
-        std::set<Node> origins;
-        Assert(d_currentOrigins.find(variable) != d_currentOrigins.end()) << "Using variable as origin that is unknown yet.";
-        getOrigins(d_currentOrigins.at(variable), origins);
-        return std::vector<Node>(origins.begin(), origins.end());
-    }
-private:
-    std::map<Node,ContractionOrigin*> d_currentOrigins;
-    std::vector<std::unique_ptr<ContractionOrigin>> d_allocations;
-};
-
-inline void print(std::ostream& os, const std::string& indent, const ContractionOriginManager::ContractionOrigin* co) {
-    if (!co->candidate.isNull()) {
-        os << indent << co->candidate << std::endl;
-    } else {
-        os << indent << "<null>" << std::endl;
-    }
-    for (const auto& o: co->origins) {
-        print(os, indent + "\t", o);
-    }
-}
-
-inline std::ostream& operator<<(std::ostream& os, const ContractionOriginManager& com) {
-    os << "ContractionOrigins:" << std::endl;
-    for (const auto& vars: com.d_currentOrigins) {
-        os << vars.first << ":" << std::endl;
-        print(os, "\t", vars.second);
-    }
-    return os;
 }
 
 class Propagator {
