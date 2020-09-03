3ee6831f7f595df1974ec0defd9e8e9176b61fab
Gereon Kremer <gkremer@stanford.edu>
2020-09-03 14:49:36 +0200

    Add comments for interval.

--- src/theory/arith/nl/icp/interval.h
+++ src/theory/arith/nl/icp/interval.h
@@ -15,10 +15,12 @@
 #ifndef CVC4__THEORY__ARITH__ICP__INTERVAL_H
 #define CVC4__THEORY__ARITH__ICP__INTERVAL_H
 
+#include "util/real_algebraic_number.h"
+
+#ifdef CVC4_POLY_IMP
 #include <poly/polyxx.h>
 
 #include "expr/node.h"
-#include "util/poly_util.h"
 
 namespace CVC4 {
 namespace theory {
@@ -26,16 +28,26 @@ namespace arith {
 namespace nl {
 namespace icp {
 
+/**
+ * Represents an interval with poly::Value bounds and origins for these bounds.
+ */
 struct Interval
 {
+  /** The lower bound */
   poly::Value lower = poly::Value::minus_infty();
+  /** Whether the lower bound is strict or weak */
   bool lower_strict = true;
+  /** The origin of the lower bound */
   Node lower_origin;
+  /** The upper bound */
   poly::Value upper = poly::Value::plus_infty();
+  /** Whether the upper bound is strict or weak */
   bool upper_strict = true;
+  /** The origin of the upper bound */
   Node upper_origin;
 };
 
+/** Print an interval */
 inline std::ostream& operator<<(std::ostream& os, const Interval& i)
 {
   return os << (i.lower_strict ? '(' : '[') << i.lower << " .. " << i.upper
@@ -47,5 +59,7 @@ inline std::ostream& operator<<(std::ostream& os, const Interval& i)
 }  // namespace arith
 }  // namespace theory
 }  // namespace CVC4
+
+#endif
 
 #endif
